61fb5ebb64497cf95f191375bfe632bbb510eafb
Nick Guenther <nguenthe@uwaterloo.ca>
2020-06-23 07:35:40 -0400

    Support uploading file objects nested any depth inside the arguments.

--- aiogqlc/client.py
+++ aiogqlc/client.py
@@ -25,57 +25,93 @@ class GraphQLClient:
     def prepare_json_data(
         cls, query: str, variables: dict = None, operation: str = None
     ) -> dict:
+        """
+        variables must be serializable; in particular,
+        it should have already been run through prepare() in case
+        it had filestreams in it.
+        """
         data = {"query": query}
         if variables:
-            data["variables"] = null_file_variables(variables)
+            data["variables"] = variables
         if operation:
             data["operationName"] = operation
         return data
 
     @classmethod
-    def prepare_files(cls, variables: dict) -> Tuple[dict, list]:
-        file_map = dict()
-        file_fields = list()
-        map_index = 0
-        for key, value in variables.items():
-            if is_file_like(value):
-                file_map[str(map_index)] = ["variables.{}".format(key)]
-                file_fields.append([str(map_index), value])
-                map_index += 1
-            elif is_file_list_like(value):
-                file_list_index = 0
-                for item in value:
-                    file_map[str(map_index)] = [
-                        "variables.{}.{}".format(key, file_list_index)
-                    ]
-                    file_fields.append([str(map_index), item])
-                    file_list_index += 1
-                    map_index += 1
-        return file_map, file_fields
+    def prepare(cls, variables: dict) -> Tuple[dict, dict, list]:
+        files = {}
+        def extract_files(path, obj):
+            """
+            recursively traverse obj, doing a deepcopy, but
+            replacing any file-like objects with nulls and
+            shunting the originals off to the side.
+            """
+            nonlocal files
+            if type(obj) is list:
+                nulled_obj = []
+                for key, value in enumerate(obj):
+                    value = extract_files(f'{path}.{key}', value)
+                    nulled_obj.append(value)
+                # TODO: merge this with dict case below. somehow.
+                return nulled_obj
+            elif type(obj) is dict:
+                nulled_obj = {}
+                for key, value in obj.items():
+                    value = extract_files(f'{path}.{key}', value)
+                    nulled_obj[key] = value
+                return nulled_obj
+            elif is_file_like(obj):
+                # extract obj from its parent and put it into files instead.
+                files[path] = obj
+                return None
+            else:
+                # base case: pass through unchanged
+                return obj
+        nulled_variables = extract_files("variables", variables)
+
+        return nulled_variables, files
 
     @classmethod
     def prepare_multipart(
-        cls, query: str, variables: dict, operation: str = None
+        cls, query: str, variables: dict, files: dict, operation: str = None
     ) -> aiohttp.FormData:
         data = aiohttp.FormData()
         operations_json = json.dumps(cls.prepare_json_data(query, variables, operation))
-        file_map, file_fields = cls.prepare_files(variables)
+
+        # separate files[path]=stream into two parts: a header and the content, with.
+        # an extra ID -- the *index* of the file in the *ordered* dict -- to link them.
+        # (the reason the spec adds the extra indirection is to allow multiple pointers
+        # to the same file. To save bandwidth):
+        file_map     = {str(i): [path] for i,path in enumerate(files)}      # header
+          # path is nested in a list because the spec allows multiple pointers to the same file.
+          # But we don't use that.
+        file_streams = {str(i): files[path] for i,path in enumerate(files)} # payload
+
         data.add_field("operations", operations_json, content_type="application/json")
         data.add_field("map", json.dumps(file_map), content_type="application/json")
-        data.add_fields(*file_fields)
+        data.add_fields(*file_streams.items())
         return data
 
     async def execute(
         self, query: str, variables: dict = None, operation: str = None
     ) -> aiohttp.ClientResponse:
         async with aiohttp.ClientSession() as session:
-            if variables and contains_file_variable(variables):
-                data = self.prepare_multipart(query, variables, operation)
-                headers = self.prepare_headers()
-            else:
+            headers = self.prepare_headers()
+
+            nulled_variables, files = self.prepare(variables)
+
+            if not files:
+                # normal GraphQL case: just send the query as JSON.
+                # nulled_variables should == variables here
                 data = json.dumps(self.prepare_json_data(query, variables, operation))
-                headers = self.prepare_headers()
                 headers[aiohttp.hdrs.CONTENT_TYPE] = "application/json"
+            else:
+                # file-uploading extension case:
+                # send the query as JSON inside the first multipart form upload
+                # followed by a header for the files, then the files themselves.
+                data = self.prepare_multipart(query, nulled_variables, files, operation)
+                #headers[aiohttp.hdrs.CONTENT_TYPE] = "multipart/form-data", but let the library handle that
+
             async with session.post(
                 self.endpoint, data=data, headers=headers
             ) as response:
