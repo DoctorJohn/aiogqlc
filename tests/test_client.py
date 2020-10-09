df44eb7a82f95ce00359fca820c8ed9236c7a69f
Kevin Amado <kamadorueda@gmail.com>
2020-10-09 08:58:00 -0500

    Add a simple client test (#9)

---
+++ tests/test_client.py
@@ -0,0 +1,43 @@
+# Local libraries
+from aiogqlc import GraphQLClient
+
+# Third party libraries
+import aiohttp
+from aioextensions import (
+    run_decorator,
+)
+
+
+@run_decorator
+async def test_client() -> None:
+    async with aiohttp.ClientSession(headers={"x-foo": "bar"}) as session:
+        client = GraphQLClient(
+            endpoint="https://swapi.graph.cool",
+            session=session,
+        )
+
+        assert client.endpoint == "https://swapi.graph.cool"
+        assert client.session is session
+        assert client.session._default_headers["x-foo"] == "bar"
+
+        response: aiohttp.ClientResponse = await client.execute("""
+            query {
+                allFilms {
+                    title
+                }
+            }
+        """)
+
+        assert await response.json() == {
+            "data": {
+                "allFilms": [
+                    {"title": "A New Hope"},
+                    {"title": "Attack of the Clones"},
+                    {"title": "The Phantom Menace"},
+                    {"title": "Revenge of the Sith"},
+                    {"title": "Return of the Jedi"},
+                    {"title": "The Empire Strikes Back"},
+                    {"title": "The Force Awakens"},
+                ],
+            },
+        }
