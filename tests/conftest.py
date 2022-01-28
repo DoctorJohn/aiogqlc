5e50f7582de3a82180cd3d4b420854a309fb761d
Jonathan Ehwald <github@ehwald.info>
2022-01-29 00:46:16 +0100

    Upgrade dependencies

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ from tests.app import create_app
 
 
 @pytest.fixture
-def graphql_session(loop, aiohttp_client):
+def graphql_session(event_loop, aiohttp_client):
     app = create_app()
-    loop.set_debug(True)
-    return loop.run_until_complete(aiohttp_client(app))
+    event_loop.set_debug(True)
+    return event_loop.run_until_complete(aiohttp_client(app))
