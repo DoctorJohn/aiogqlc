3426177ca2bd20cea6b083b31756bce326bd7450
Jonathan Ehwald <github@ehwald.info>
2025-10-18 02:27:36 +0200

    Recommend using a pytest fixture when testing

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,12 +1,3 @@
-# Testing
-
-There is no need to mock `aiogqlc` in your tests.
-Instead you might want to mock your GraphQL backend.
-This can be done by providing a `aiohttp.web.Application`.
-
-The following example shows an example based on `pytest`, `pytest-asyncio`, and `pytest-aiohttp` using a mock GraphQL backend:
-
-```python
 import aiohttp
 import aiohttp.web
 import pytest
@@ -32,8 +23,3 @@ async def graphql_client(aiohttp_client):
 async def test_ping_query(graphql_client):
     response = await graphql_client.execute("query { ping }")
     assert await response.json() == {"data": {"ping": "pong"}}
-
-```
-
-In fact, the `aiogqlc` test suite itself is based on this approach.
-Take a look at [the aiogqlc tests directory](https://github.com/DoctorJohn/aiogqlc/tree/main/tests) for more advanced examples.
